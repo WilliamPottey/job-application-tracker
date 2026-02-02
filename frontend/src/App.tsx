9d771e60980c8a7d34d14cc73bb2b56f6aa152b8
wpottey <wpottey@meditech.com>
2026-02-02 12:35:42 -0500

    Initialize frontned using Vite + React + Typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div style={{ padding: "2rem" }}>
+        <h1>Job Application Tracker</h1>
+        <p>Frontend is alive!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
