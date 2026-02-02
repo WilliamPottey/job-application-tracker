7f04bfcecf16c0a9cfc6aa77e7c4461c0cb023c8
wpottey <wpottey@meditech.com>
2026-02-02 16:40:21 -0500

    Add basic routing for login and applications pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import { login } from "./api/auth";
 import { useAuth } from "./context/useAuth";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Applications from "./pages/Applications";
 
 
 
@@ -20,10 +23,18 @@ function App() {
 
   return (
     <>
-      <div style={{ padding: "2rem" }}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Navigate to="/login"/> } />
+        <Route path="/login" element={ <Login /> } />
+        <Route path="/applications" element={ <Applications/> } />
+        
+      </Routes>
+    </BrowserRouter>
+      {/* <div style={{ padding: "2rem" }}>
         <h1>Job Application Tracker</h1>
         <button onClick={onClickLogin}>Test Login</button>
-      </div>
+      </div> */}
     </>
   )
 }
---
+++ frontend/src/pages/Applications.tsx
@@ -0,0 +1,7 @@
+export default function Applications() {
+    return (
+        <>
+            <h1>Applications Dashboard</h1>
+        </>
+    )
+}
